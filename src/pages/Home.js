5dad721a9efcf99cf19709937cb9769ccc8a9c69
PeterMatthew <pedro_mateus_12@hotmail.com>
2022-05-03 23:18:51 -0300

    feat: add Map usage on Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,30 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import Map from "../components/Map";
+import { Marker } from "react-leaflet";
 
 function Home() {
+  const [ position, setPosition ] = useState([-9.576503756696837, -35.75611430266957]);
+  const [ showPositionMarker, setShowPositionMarker ] = useState(false);
+
+  useEffect(() => {
+    if(navigator.geolocation) navigator.geolocation.getCurrentPosition(showPosition);
+    else console.log("Geolocation is not supported by this browser.");
+  }, []);
+
+  function showPosition(position) {
+    setPosition([position.coords.latitude, position.coords.longitude]);
+    setShowPositionMarker(true);
+  }
+
   return (
-    <Box>
-      Home
-    </Box>
+    <Flex align='center' justify='center'>
+      <Map position={position} height='500px' width='700px'>
+        {showPositionMarker &&
+          (<Marker position={position}></Marker>)
+        }
+      </Map>
+    </Flex>
   );
 }
 
