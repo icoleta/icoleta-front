09121c18f16ac5262edb92482d39dae6e42faf22
Fernando Lins <fernandolins088@gmail.com>
2022-06-01 15:21:42 -0300

    feat: add create point page

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import ListPoints from "./pages/ListPoints";
 import MapPoints from "./pages/MapPoints";
 import Body from "./components/Body";
 import AdminEntidade from "./pages/AdminEntidade";
+import CreatePoint from './pages/CreatePoint'
 
 function Paths() {
   return (
@@ -16,6 +17,7 @@ function Paths() {
           <Route path="/map" element={<MapPoints />} />
           <Route path="/list-points" element={<ListPoints />} />
           <Route exact path="/entidade/admin" element={<AdminEntidade />} />
+          <Route exact path="/point/create" element={<CreatePoint />} />
         </Routes>
       </Body>
     </BrowserRouter>
