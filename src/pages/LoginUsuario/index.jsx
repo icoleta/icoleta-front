b77728c3abb9c2f746430650ab676d3818d94d1c
techpril <priscila.teodorio2015@gmail.com>
2022-06-13 10:03:11 -0300

    adding user part

--- src/pages/LoginUsuario/index.jsx
+++ src/pages/LoginUsuario/index.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 import './style.css'
 
-const LoginEntidade = () => {
+const LoginUsuario = () => {
   return (
     <div className="container-login">
       <form>
 
-        <p className="titulo-cadastro">Acesso ao sistema</p>
+        <p className="titulo-cadastro">Acesso ao sistema usuário</p>
 
         <label htmlFor="email">Email:</label>
         <input type="mail" id="email" name="email"></input>
@@ -17,15 +17,16 @@ const LoginEntidade = () => {
         <input type="password" id="senha" name="senha"></input>
         <br></br>
 
-        <a href='/' className="link-recuperacao">Esqueci minha senha</a>
+        <a href='/usuario/recuperar' className="link-recuperacao">Esqueci minha senha</a>
 
         <div className="btn-flex-login">
-          <button type="submit" className="btn-login" value="Submit"> Login </button>
-          <button className="btn-cadastro-login" type="button"><a href="/entidade/cadastro">Cadastrar</a></button>
+          <button type="submit" className="btn-login" value="Submit"> Entrar </button>
+          <button className="btn-cadastro-login" type="button"><a href="/usuario/cadastrar">Cadastrar</a></button>
         </div>
+
       </form>
     </div>
   )
 }
 
-export default LoginEntidade
+export default LoginUsuario
