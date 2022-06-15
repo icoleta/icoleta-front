1577a6075df2abd5e20edccd88fca8aa8c8c18f7
PeterMatthew <pedro_mateus_12@hotmail.com>
2022-06-15 11:38:28 -0300

    fix styles

--- src/pages/EditarEntidade/index.jsx
+++ src/pages/EditarEntidade/index.jsx
@@ -1,36 +1,85 @@
-import React from 'react'
+import React from "react";
 
-import './style.css'
+import "./style.css";
 
 const EditarEntidade = () => {
   return (
-    <div className="container-editar">
-      <form>
+    <div className="flex justify-center items-center h-5/6 flex-col">
+      <form className="w-1/6">
+        <p className="text-lg mb-4">Editar entidade</p>
 
-        <p className="titulo-editar">Editar entidade selecionada</p>
+        <label
+          htmlFor="cnpj"
+          className="block mb-2 text-sm font-medium text-gray-900"
+        >
+          CNPJ
+        </label>
+        <input
+          type="text"
+          id="cnpj"
+          name="cnpj"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        ></input>
 
-        <label htmlFor="cnpj">CNPJ:</label>
-        <input type="text" id="cnpj" name="cnpj"></input>
         <br></br>
 
-        <label htmlFor="nome">Nome:</label>
-        <input type="text" id="nome" name="nome"></input>
+        <label
+          htmlFor="nome"
+          className="block mb-2 text-sm font-medium text-gray-900"
+        >
+          Nome
+        </label>
+        <input
+          type="text"
+          id="nome"
+          name="nome"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        ></input>
+
         <br></br>
 
-        <label htmlFor="email">Email:</label>
-        <input type="mail" id="email" name="email"></input>
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900"
+        >
+          Email
+        </label>
+        <input
+          type="mail"
+          id="email"
+          name="email"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        ></input>
+
         <br></br>
 
-        <label htmlFor="senha">Senha:</label>
-        <input type="password" id="senha" name="senha"></input>
+        <label
+          htmlFor="senha"
+          className="block mb-2 text-sm font-medium text-gray-900"
+        >
+          Senha
+        </label>
+        <input
+          type="password"
+          id="senha"
+          name="senha"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        ></input>
+
         <br></br>
 
         <div class="btn-flex">
-          <button type="submit" className="btn-editar" value="Submit">Editar</button>
+          <button
+            type="submit"
+            className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+            value="Submit"
+          >
+            Editar
+          </button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default EditarEntidade
+export default EditarEntidade;
