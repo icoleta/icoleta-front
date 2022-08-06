0744f19ce4bacf854ba49549fa145326ed09d7a3
techpril <priscila.teodorio2015@gmail.com>
2022-08-06 10:02:15 -0300

    admin page with tailwind

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -4,120 +4,44 @@ import './style.css'
 const AdminEntidade = () => {
   return (
     <>
-      <main class="main-admin">
-        <div class="main-admin-container container-md">
-          <div class="container-entidade">
-            <div class="logo-entidade">
-              <img class="border-b  w-8 mx-2 " src="/static/media/recycle_icon.2fb95d3372a9538a18df3a05b0ac0b9f.svg"></img>
-              <h3 class="h5">Área do Admin</h3>
+    <main class="main-admin">
+
+        <div class="bg-gray-100 sm:grid grid-cols-5 grid-rows-2 px-4 py-6 min-h-full lg:min-h-screen space-y-6 sm:space-y-0 sm:gap-4">
+
+            <div class="h-96 col-span-4  bg-olive-green rounded-md flex items-center">
+                <div class="ml-20 mr-20">
+                    <h2 class="text-white text-4xl">IColeta</h2>
+                    <p class="text-indigo-100 mt-4 font-thin tracking-wider leading-7">Alguma frase motivacional para quem estiver cuidando do projeto 
+                    não se esquecer do objetivo principal que é ajudar e incentivar a sustentabilidade, aprender e aperfeiçoar os conhecimentos em desenvolvimento web, 
+                    estar sempre melhorando o visual e a gamificação do app para que os usuários se sintam motivados, incluidos e bem informados sobre a coleta de 
+                    resíduos perigosos e reciclaveis para se sentirem instigados a proteger a natureza.</p>
+
+                    <a href="#" class="uppercase inline-block mt-8 text-sm bg-white py-2 px-4 rounded font-semibold hover:bg-indigo-100">Algo</a>
+                </div>
             </div>
-          </div>
 
-        </div>
+            <div class="h-96 col-span-1 ">
+                <div class="bg-white py-3 px-4 mb-4 rounded-lg flex justify-around items-center ">
+                    <input type="text" placeholder="Buscar" class=" bg-gray-100 rounded-md  outline-none pl-2 ring-indigo-700 w-full mr-2 p-2" />
+                    <span><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor ">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg></span>
+                </div>
 
-        <div class="container flex justify-center">
-            <div class="flex flex-col">
-                <div class="w-full">
-                    <div class="border-b border-gray-200 shadow">
-                        <table>
-                            <thead class="bg-gray-50">
-                                <tr>
-                                    <th class="px-6 py-2 text-xs text-gray-500">
-                                        ID
-                                    </th>
-                                    <th class="px-6 py-2 text-xs text-gray-500">
-                                        Nome do ponto
-                                    </th>
-                                    <th class="px-6 py-2 text-xs text-gray-500">
-                                        Entidade Responsável
-                                    </th>
-                                    <th class="px-6 py-2 text-xs text-gray-500">
-                                        Status
-                                    </th>
-                                    <th class="px-6 py-2 text-xs text-gray-500">
-                                        Editar
-                                    </th>
-                                    <th class="px-6 py-2 text-xs text-gray-500">
-                                        Deletar
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody class="bg-white">
-                                <tr class="whitespace-nowrap">
-                                    <td class="px-6 py-4 text-sm text-gray-500">
-                                        1
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <div class="text-sm text-gray-900">
-                                            Jon doe
-                                        </div>
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <div class="text-sm text-gray-500">jhondoe@example.com</div>
-                                    </td>
-                                    <td class="px-6 py-4 text-sm text-gray-500">
-                                        2021-1-12
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <a href="#" class="px-4 py-1 text-sm text-white  bg-green-600 rounded">Editar</a>
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <a href="#" class="px-4 py-1 text-sm text-white bg-red-400 rounded">Deletar</a>
-                                    </td>
-                                </tr>
-                                <tr class="whitespace-nowrap">
-                                    <td class="px-6 py-4 text-sm text-gray-500">
-                                        1
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <div class="text-sm text-gray-900">
-                                            Jon doe
-                                        </div>
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <div class="text-sm text-gray-500">jhondoe@example.com</div>
-                                    </td>
-                                    <td class="px-6 py-4 text-sm text-gray-500">
-                                        2021-1-12
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <a href="#" class="px-4 py-1 text-sm text-white  bg-green-600 rounded">Editar</a>
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <a href="#" class="px-4 py-1 text-sm text-white bg-red-400 rounded">Deletar</a>
-                                    </td>
-                                </tr>
-                                <tr class="whitespace-nowrap">
-                                    <td class="px-6 py-4 text-sm text-gray-500">
-                                        1
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <div class="text-sm text-gray-900">
-                                            Jon doe
-                                        </div>
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <div class="text-sm text-gray-500">jhondoe@example.com</div>
-                                    </td>
-                                    <td class="px-6 py-4 text-sm text-gray-500">
-                                        2021-1-12
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <a href="#" class="px-4 py-1 text-sm text-white bg-green-600 rounded">Editar</a>
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        <a href="#" class="px-4 py-1 text-sm text-white bg-red-400 rounded">Deletar</a>
-                                    </td>
-                                </tr>
-
-                            </tbody>
-                        </table>
+                <div class="bg-white  rounded-md">
+                    <h1 class="text-center text-xl bg-white py-2 rounded-md border-b-2 cursor-pointer  text-olive-green">Área do ADMIN</h1>
+                    <div class="bg-white rounded-md list-none  text-center ">
+                        <li class="py-3 rounded-lg border-b-2 hover:bg-sunset-orange"><a href="#" class="list-none">Ranking de usuários</a></li>
+                        <li class="py-3 rounded-lg border-b-2 hover:bg-sunset-orange"><a href="#" class="list-none">Histórico de coletas</a></li>
+                        <li class="py-3 rounded-lg border-b-2 hover:bg-sunset-orange"><a href="#" class="list-none">Cadastrar coleta</a></li>
+                        <li class="py-3 rounded-lg border-b-2 hover:bg-sunset-orange"><a href="#" class="list-none">Cadastrar entidade</a></li>
+                        <li class="py-3 rounded-lg hover:bg-sunset-orange"><a href="#" class="list-none">Cadastrar ponto</a></li>
                     </div>
                 </div>
             </div>
-          </div>
+        </div>
 
-      </main>
+    </main>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'olive-green': '#4B9093',
+        'sunset-orange': '#F59A73',
+        'almost-white': '#F4F7F8',
+      },
+    },
   },
   plugins: [
     function ({ addVariant }) {
