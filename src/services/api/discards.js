b40e1e6a5fe49389e9e5a1845cbbe3640a3dad48
PeterMatthew <pedro_mateus_12@hotmail.com>
2022-08-14 18:51:48 -0300

    add discards requests

---
+++ src/services/api/discards.js
@@ -0,0 +1,18 @@
+import { api } from "./request";
+
+const discardApi = {
+  async fetchDiscards() {
+    return api.get("/discards");
+  },
+  async fetchDiscard(id) {
+    return api.get(`/discards/${id}`);
+  },
+  async createDiscard(payload) {
+    return api.post("/discards", payload);
+  },
+  async verifyDiscard(id, payload) {
+    return api.patch(`/discards/${id}`, payload);
+  },
+};
+
+export default discardApi;
