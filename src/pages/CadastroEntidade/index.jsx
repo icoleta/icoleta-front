199f8f4460c743f010468bb14a3bdf201dc46f67
PeterMatthew <pedro_mateus_12@hotmail.com>
2022-08-17 16:20:32 -0300

    fix create entity

--- src/pages/CadastroEntidade/index.jsx
+++ src/pages/CadastroEntidade/index.jsx
@@ -16,31 +16,33 @@ const CadastroEntidade = () => {
   );
 
   async function whenSubmitted() {
-    await axios.get('http://localhost:8000/sanctum/csrf-cookie')
     await companyApi.createCompany(values);
     alert("Entidade criada");
     navigate("/");
   }
 
   return (
-
     <div class="m-8">
       <div class="text-center mt-8">
-      
-      <h2 class="text-4xl tracking-tight">
-        Cadastro de Entidade
-      </h2>
-      
-    </div>
-    <div class="flex justify-center my-2 mx-4 md:mx-0">
-      <form onSubmit={handleSubmit} class="w-full max-w-xl bg-white rounded-lg shadow-md p-6">
-        <fieldset className="font-semibold my-2 text-lg text-center">
-              Informações da Entidade
-        </fieldset>
-        
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-full px-3 mb-6">
-              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="name">Nome</label>
+        <h2 class="text-4xl tracking-tight">Cadastro de Entidade</h2>
+      </div>
+      <div class="flex justify-center my-2 mx-4 md:mx-0">
+        <form
+          onSubmit={handleSubmit}
+          class="w-full max-w-xl bg-white rounded-lg shadow-md p-6"
+        >
+          <fieldset className="font-semibold my-2 text-lg text-center">
+            Informações da Entidade
+          </fieldset>
+
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-full px-3 mb-6">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="name"
+              >
+                Nome
+              </label>
               <Input
                 type="text"
                 id="name"
@@ -48,10 +50,15 @@ const CadastroEntidade = () => {
                 onChange={handleChange}
                 errors={errors}
               />
-          </div>
+            </div>
 
-          <div class="w-full md:w-full px-3 mb-6">
-              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="cnpj">CNPJ</label>
+            <div class="w-full md:w-full px-3 mb-6">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="cnpj"
+              >
+                CNPJ
+              </label>
               <Input
                 type="text"
                 id="cnpj"
@@ -59,10 +66,15 @@ const CadastroEntidade = () => {
                 onChange={handleChange}
                 errors={errors}
               />
-          </div>
+            </div>
 
-          <div class="w-full md:w-full px-3 mb-6">
-              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="phone">Telefone</label>
+            <div class="w-full md:w-full px-3 mb-6">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="phone"
+              >
+                Telefone
+              </label>
               <Input
                 type="text"
                 id="phone"
@@ -70,14 +82,19 @@ const CadastroEntidade = () => {
                 onChange={handleChange}
                 errors={errors}
               />
+            </div>
           </div>
-        </div>
           <fieldset className="font-semibold my-2 text-lg text-center">
             Informações de Login
           </fieldset>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-full px-3 mb-6">
-              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="email">Email da entidade</label>
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-full px-3 mb-6">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="email"
+              >
+                Email da entidade
+              </label>
               <Input
                 type="mail"
                 id="email"
@@ -85,10 +102,15 @@ const CadastroEntidade = () => {
                 onChange={handleChange}
                 errors={errors}
               />
-          </div>
+            </div>
 
-          <div class="w-full md:w-full px-3 mb-6">
-              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="password">Senha</label>
+            <div class="w-full md:w-full px-3 mb-6">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="password"
+              >
+                Senha
+              </label>
               <Input
                 type="password"
                 id="password"
@@ -96,22 +118,22 @@ const CadastroEntidade = () => {
                 onChange={handleChange}
                 errors={errors}
               />
+            </div>
           </div>
-        </div>
 
-
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-full px-3 mb-6">
-              <button type="submit" class="appearance-none block w-full bg-olive-green text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-olive-green-dark focus:outline-none focus:bg-white focus:border-gray-500">Entrar</button>
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-full px-3 mb-6">
+              <button
+                type="submit"
+                class="appearance-none block w-full bg-olive-green text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-olive-green-dark focus:outline-none focus:bg-white focus:border-gray-500"
+              >
+                Entrar
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
-  </div>
-
-  
-</div>
-
-
+        </form>
+      </div>
+    </div>
   );
 };
 
