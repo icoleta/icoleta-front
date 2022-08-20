13f6acb7911b0be10eb1a72c9689daf7bd842b58
Fernando Lins <fernandolins088@gmail.com>
2022-08-20 10:23:57 -0300

    Update ranking to use api data

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,8 +1,18 @@
-import React from "react";
-import { FaMapMarkedAlt, FaClipboardList } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useEffect } from "react";
+
+import othersApi from '../services/api/others'
 
 function Ranking() {
+  const [ranking, setRanking] = useState([])
+
+  useEffect(() => {
+    othersApi.fetchRanking()
+    .then(res => {
+      setRanking(res.data.data.ranking)
+    })
+  }, [])
+  
   return (
     <div className="flex flex-col justify-center px-64">
       <h3 className="text-lg my-8 text-center font-semibold text-slate-900">
@@ -22,51 +32,29 @@ function Ranking() {
                 Quantidade doada{" "}
                 <span className="text-gray-400 font-normal">(kg)</span>
               </th>
-              <th scope="col" className="py-3 px-6">
+              {/* <th scope="col" className="py-3 px-6">
                 Material mais doado
-              </th>
+              </th> */}
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white border-b">
-              <th
-                scope="row"
-                className="py-4 px-6 font-bold text-lg text-orange-500 whitespace-nowrap"
-              >
-                1º
-              </th>
-              <td className="py-4 px-6 text-gray-700 font-semibold">
-                Pessoa 1
-              </td>
-              <td className="py-4 px-6">2.5kg</td>
-              <td className="py-4 px-6">papel</td>
-            </tr>
-            <tr className="bg-white border-b">
-              <th
-                scope="row"
-                className="py-4 px-6 font-bold text-lg text-orange-500 whitespace-nowrap"
-              >
-                2º
-              </th>
-              <td className="py-4 px-6 text-gray-700 font-semibold">
-                Pessoa 2
-              </td>
-              <td className="py-4 px-6">1.2kg</td>
-              <td className="py-4 px-6">plástico</td>
-            </tr>
-            <tr className="bg-white">
-              <th
-                scope="row"
-                className="py-4 px-6 font-bold text-lg text-orange-500 whitespace-nowrap"
-              >
-                3º
-              </th>
-              <td className="py-4 px-6 text-gray-700 font-semibold">
-                Pessoa 3
-              </td>
-              <td className="py-4 px-6">0.8kg</td>
-              <td className="py-4 px-6">metal</td>
-            </tr>
+            {
+              ranking.map((pos, index) => (
+                <tr className="bg-white border-b">
+                  <th
+                    scope="row"
+                    className="py-4 px-6 font-bold text-lg text-orange-500 whitespace-nowrap"
+                  >
+                    {index + 1}
+                  </th>
+                  <td className="py-4 px-6 text-gray-700 font-semibold">
+                    {pos.person.name}
+                  </td>
+                  <td className="py-4 px-6">{pos.sum}</td>
+                  {/* <td className="py-4 px-6">papel</td> */}
+                </tr>
+              ))
+            }
           </tbody>
         </table>
       </div>
