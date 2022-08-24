7e547b33c7210342203743de60e068f883e6cea4
Fernando Lins <fernandolins088@gmail.com>
2022-08-24 03:12:33 -0300

    Fix Header: increase links clickable area

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,102 +29,28 @@ function Header() {
         </div>
       </Link>
       <div className="flex justify-center">
-        <div className="child:ml-4 flex items-center text-slate-700 font-semibold text-sm">
-          {/* <Link to="/mapa">Mapa</Link> */}
-          {/* <Link to="/lista-de-pontos">Pontos de coleta</Link> */}
-          {/* {signed ? (
-            <Link to="/ponto/criar">Cadastrar ponto</Link>
+        {
+          signed ? (
+            <div className="child:ml-4 flex items-center text-slate-700 font-semibold text-sm">
+              {user.isCompany ? (
+                <Link to="/entidade/admin" className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75">Seu perfil</Link>
+              ) : (
+                <Link to="/perfil" className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75">Seu perfil</Link>
+              )}
+              <Link to="/ranking" className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75">Ranking</Link>
+              <button type="button" className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75 font-semibold" onClick={logoutWrapper}>
+                Deslogar
+              </button>
+            </div>
           ) : (
-            <Link to="/entidade/login">Cadastrar ponto</Link>
-          )} */}
-        </div>
-        {signed ? (
-          // <div className="mx-8">
-          //   <div
-          //     onClick={() => setIsDropdownOpen(true)}
-          //     className="relative cursor-pointer w-10 h-10 overflow-hidden bg-gray-100 rounded-full"
-          //   >
-          //     <svg
-          //       className="absolute w-12 h-12 text-gray-400 -left-1"
-          //       fill="currentColor"
-          //       viewBox="0 0 20 20"
-          //       xmlns="http://www.w3.org/2000/svg"
-          //     >
-          //       <path
-          //         fill-rule="evenodd"
-          //         d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-          //         clip-rule="evenodd"
-          //       ></path>
-          //     </svg>
-          //   </div>
-          //   <div
-          //     id="dropdown"
-          //     ref={wrapperRef}
-          //     className={
-          //       (isDropdownOpen ? "" : "hidden ") +
-          //       "absolute z-10 bg-white divide-y divide-gray-100 rounded shadow w-44"
-          //     }
-          //   >
-          //     <ul
-          //       className="py-1 text-sm text-gray-700"
-          //       aria-labelledby="dropdownDefault"
-          //     >
-          //       <li>
-          //         <a
-          //           href="/entidade/admin"
-          //           className="block px-4 py-2 hover:bg-gray-100"
-          //         >
-          //           Seu perfil
-          //         </a>
-          //       </li>
-          //       <li>
-          //         <a
-          //           href="/entidade/edicao"
-          //           className="block px-4 py-2 hover:bg-gray-100"
-          //         >
-          //           Editar dados
-          //         </a>
-          //       </li>
-          //       <li>
-          //         <a
-          //           href="/ponto/criar"
-          //           className="block px-4 py-2 hover:bg-gray-100"
-          //         >
-          //           Cadastrar ponto
-          //         </a>
-          //       </li>
-          //       <li>
-          //         <a href="#" className="block px-4 py-2 hover:bg-gray-100">
-          //           Sair
-          //         </a>
-          //       </li>
-          //     </ul>
-          //   </div>
-          // </div>
-          <div className="child:ml-4 flex items-center text-slate-700 font-semibold text-sm ">
-            {/* <Link to="/usuario/cadastrar">Criar conta</Link> */}
-            {user.isCompany ? (
-              <Link to="/entidade/admin">Seu perfil</Link>
-            ) : (
-              <Link to="/perfil">Seu perfil</Link>
-            )}
-            {/* <Link to="/entidade/edicao">Editar dados</Link> */}
-            {/* <Link to="/ponto/criar">Cadastrar ponto</Link> */}
-            <Link to="/ranking">Ranking</Link>
-            <button type="button" onClick={logoutWrapper}>
-              Deslogar
-            </button>
-          </div>
-        ) : (
-          <div className="child:ml-4 flex flex-col items-center text-slate-700 font-semibold text-sm phone:flex-row">
-            {/* <Link to="/usuario/cadastrar">Criar conta</Link> */}
-
-            <button class="w-full rounded-md mb-2 bg-sunset-orange  py-2 text-white hover: bg-sunset-orange hover:bg-sunset-orange-dark  duration-75"><Link to="/ranking"  class="m-8">Ranking</Link></button>
-            <button class="w-full rounded-md mb-2 bg-sunset-orange  py-2 text-white hover: bg-sunset-orange hover:bg-sunset-orange-dark duration-75"><Link to="/usuario/cadastrar"  class="m-8">Usuário</Link></button>
-            <button class="w-full rounded-md mb-2 bg-sunset-orange  py-2 text-white hover: bg-sunset-orange hover:bg-sunset-orange-dark duration-75"><Link to="/entidade/registro"  class="m-8">Entidade</Link></button>
-            <button class="w-full rounded-md mb-2 bg-sunset-orange  py-2 text-white hover: bg-sunset-orange hover:bg-sunset-orange-dark  duration-75"><Link to="/login"  class="m-8">Login</Link></button>
-          </div>
-        )}
+            <div className="child:ml-5 flex flex-col items-center text-slate-700 font-semibold text-sm phone:flex-row">
+              <Link to="/ranking" className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75">Ranking</Link>
+              <Link to="/usuario/cadastrar" className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75">Usuário</Link>
+              <Link to="/entidade/registro" className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75">Entidade</Link>
+              <Link to="/login" className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75">Login</Link>
+            </div>
+          )
+        }
       </div>
     </div>
   );
