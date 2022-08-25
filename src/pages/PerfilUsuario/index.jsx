aae7ba5de4a9937ec17982d6ee295dd572a0b076
Fernando Lins <fernandolins088@gmail.com>
2022-08-25 01:34:04 -0300

    Fix react error caused by class keyword (changed to className)

--- src/pages/PerfilUsuario/index.jsx
+++ src/pages/PerfilUsuario/index.jsx
@@ -2,54 +2,52 @@ import React from "react";
 
 function PerfilUsuario() {
     return (
- 
-
-            <div class="bg-gray-100 sm:grid grid-cols-5 grid-rows-2 px-4 py-6 min-h-full space-y-6 sm:space-y-0 sm:gap-4">
+            <div className="bg-gray-100 sm:grid grid-cols-5 grid-rows-2 px-4 py-6 min-h-full space-y-6 sm:space-y-0 sm:gap-4">
 
 
-                <div class="col-span-8 items-center"> 
+                <div className="col-span-8 items-center"> 
 
-                <div class=" flex col-span-4  bg-olive-green rounded-md flex items-center">
-                    <div class="flex flex-col w-full mx-2 py-12 justify-between desktop:flex-row phone:mx-20">
-                        <h2 class="text-white text-4xl">Bem vindo! Essa é sua hash para descarte:</h2>
+                <div className=" flex col-span-4  bg-olive-green rounded-md flex items-center">
+                    <div className="flex flex-col w-full mx-2 py-12 justify-between desktop:flex-row phone:mx-20">
+                        <h2 className="text-white text-4xl">Bem vindo! Essa é sua hash para descarte:</h2>
 
 
-                        <a href="#" class="mt-4 w-fit uppercase inline-block text-sm bg-sunset-orange text-white py-2 px-4 rounded font-semibold hover:bg-indigo-100">97a90717ca617830d1af7c02e172a6aa4405ad52</a>
+                        <a href="#" className="mt-4 w-fit uppercase inline-block text-sm bg-sunset-orange text-white py-2 px-4 rounded font-semibold hover:bg-indigo-100">97a90717ca617830d1af7c02e172a6aa4405ad52</a>
                     </div>
                 </div>
 
-                    <div class="container px-5 pt-20 mx-auto">
-                        <div class="flex flex-wrap -m-4 text-center">
-                            <div class="p-4 tablet:w-1/5 w-1/2">
-                                <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">2.7Kg</h2>
-                                <p class="leading-relaxed">Papel</p>
+                    <div className="container px-5 pt-20 mx-auto">
+                        <div className="flex flex-wrap -m-4 text-center">
+                            <div className="p-4 tablet:w-1/5 w-1/2">
+                                <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">2.7Kg</h2>
+                                <p className="leading-relaxed">Papel</p>
                             </div>
 
-                            <div class="p-4 tablet:w-1/5 w-1/2">
-                                <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">2.7Kg</h2>
-                                <p class="leading-relaxed">Metal</p>
+                            <div className="p-4 tablet:w-1/5 w-1/2">
+                                <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">2.7Kg</h2>
+                                <p className="leading-relaxed">Metal</p>
                             </div>
 
-                            <div class="p-4 tablet:w-1/5 w-1/2">
-                                <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">2.7Kg</h2>
-                                <p class="leading-relaxed">Plástico</p>
+                            <div className="p-4 tablet:w-1/5 w-1/2">
+                                <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">2.7Kg</h2>
+                                <p className="leading-relaxed">Plástico</p>
                             </div>
 
-                            <div class="p-4 tablet:w-1/5 w-1/2">
-                                <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">2.7Kg</h2>
-                                <p class="leading-relaxed">Vidro</p>
+                            <div className="p-4 tablet:w-1/5 w-1/2">
+                                <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">2.7Kg</h2>
+                                <p className="leading-relaxed">Vidro</p>
                             </div>
 
-                            <div class="p-4 tablet:w-1/5 w-1/2">
-                                <h2 class="title-font font-medium sm:text-4xl text-3xl text-gray-900">2.7Kg</h2>
-                                <p class="leading-relaxed">Pilha</p>
+                            <div className="p-4 tablet:w-1/5 w-1/2">
+                                <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">2.7Kg</h2>
+                                <p className="leading-relaxed">Pilha</p>
                             </div>
                             
                         </div>
                     </div>
 
-                <div class="text-center mt-12  mb-6">
-                    <h2 class="text-4xl tracking-tight text-olive-green">
+                <div className="text-center mt-12  mb-6">
+                    <h2 className="text-4xl tracking-tight text-olive-green">
                         Seu histórico de descartes
                     </h2>
                 </div>
