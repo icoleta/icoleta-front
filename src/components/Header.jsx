de8dc17c8f919299004d7a2efcdfdf671327e665
PeterMatthew <pedro_mateus_12@hotmail.com>
2022-09-03 18:46:27 -0300

    show entity register button only on user register page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import svg from "../assets/recycle_icon.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useAuth } from "../contexts/auth";
 import useOutsideClick from "../hooks/useOutsideClick";
 import { useNavigate } from "react-router-dom";
@@ -10,6 +10,7 @@ import "../pages/CadastroEntidade/style.css";
 function Header() {
   const { signed, user, Logout } = useAuth();
   const wrapperRef = useRef(null);
+  const location = useLocation();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   useOutsideClick(wrapperRef, () => setIsDropdownOpen(false));
 
@@ -31,85 +32,80 @@ function Header() {
         </div>
       </Link>
       <div className="flex justify-center">
-        {
-          signed ? (
-            <div className="child:ml-4 flex items-center text-slate-700 font-semibold text-sm">
-              {
-                user.role !== 'admin' && user.role !== 'company' && (
-                  <Link
-                    to="/perfil"
-                    className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75"
-                  >
-                    Dashboard
-                  </Link>
-                )
-              }
-              {
-                user.role === 'company' && (
-                  <Link
-                    to="/entidade/admin"
-                    className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75"
-                  >
-                    Dashboard
-                  </Link>
-                )
-              }
-              {
-                user.role === 'admin' && (
-                  <Link
-                    to="/admin"
-                    className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75"
-                  >
-                    Admin
-                  </Link>
-                )
-              }
+        {signed ? (
+          <div className="child:ml-4 flex items-center text-slate-700 font-semibold text-sm">
+            {user.role !== "admin" && user.role !== "company" && (
               <Link
-                to="/ranking"
+                to="/perfil"
                 className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75"
               >
-                Ranking
+                Dashboard
               </Link>
-              <button
-                type="button"
-                className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75 font-semibold"
-                onClick={logoutWrapper}
-              >
-                Sair
-              </button>
-            </div>
-          ) : (
-            <div className="child:ml-5 flex flex-col items-center text-slate-700 font-semibold text-sm phone:flex-row">
+            )}
+            {user.role === "company" && (
               <Link
-                to="/ranking"
+                to="/entidade/admin"
                 className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75"
               >
-                Ranking
+                Dashboard
               </Link>
+            )}
+            {user.role === "admin" && (
+              <Link
+                to="/admin"
+                className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75"
+              >
+                Admin
+              </Link>
+            )}
+            <Link
+              to="/ranking"
+              className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75"
+            >
+              Ranking
+            </Link>
+            <button
+              type="button"
+              className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75 font-semibold"
+              onClick={logoutWrapper}
+            >
+              Sair
+            </button>
+          </div>
+        ) : (
+          <div className="child:ml-5 flex flex-col items-center text-slate-700 font-semibold text-sm phone:flex-row">
+            <Link
+              to="/ranking"
+              className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75"
+            >
+              Ranking
+            </Link>
 
+            {location.pathname !== "/usuario/cadastrar" && (
               <Link
                 to="/usuario/cadastrar"
                 className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75 rounded-t py-2 px-4 block whitespace-no-wrap"
               >
                 Cadastro Usuário
               </Link>
+            )}
 
+            {location.pathname === "/usuario/cadastrar" && (
               <Link
                 to="/entidade/registro"
                 className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75 rounded-t py-2 px-4 block whitespace-no-wrap"
               >
                 Cadastro Entidade
-              </Link> 
-
-              <Link
-                to="/login"
-                className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75"
-              >
-                Entrar
               </Link>
-            </div>
-          )
-        }
+            )}
+            <Link
+              to="/login"
+              className="rounded-md bg-sunset-orange py-2 px-6 mb-2 text-white hover:bg-sunset-orange-dark duration-75"
+            >
+              Entrar
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
