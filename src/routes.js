2cb4de344c6597ae486df76f14817935726a3d50
Jvictor-7 <jvcl@ic.ufal.br>
2024-03-21 21:50:31 -0300

    fix: Bug no link 'sobre' do header resolvido, antes ao clicar neste link fora da rota home, não era redirecionado para o elemento de id #sobre-projeto na Home.

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, 
+import {
+  BrowserRouter, Routes, Route,
   // Navigate 
 } from "react-router-dom";
 import { useAuth } from "./contexts/auth";
