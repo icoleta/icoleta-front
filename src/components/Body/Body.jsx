f67fc054b47406a52709d1935d1196c79cb847f1
mateussf99 <mateus.ms65@gmail.com>
2024-10-03 15:54:15 -0300

    :sparkles: feat:qrcode scanner aumentanto o tamanh od butão

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -9,7 +9,7 @@ function Body(props) {
     <>
       <Header />
       <div className="relative">
-        <div className="privada fixed bottom-10 right-10 bg-sunset-orange border border-olive-green rounded-full w-10 h-10 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 focus:ring-1 focus:ring-orange-300">
+        <div className="fixed flex items-center justify-center bottom-10 right-10 bg-sunset-orange border border-olive-green rounded-full w-[70px] h-[70px] ">
           <Qrcode />
         </div>
         <div className="min-h-screen">{props.children}</div>
