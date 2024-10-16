ee9280cadc686cb658f21b835fa35d38379da0a0
mateussf99 <mateus.ms65@gmail.com>
2024-10-16 17:52:34 -0300

    :sparkles: feat:qrcode com a logica de so aparecer quando estiver logado

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-
+import { useAuth } from "../../contexts/auth";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Qrcode from "../Qrcode/Qrcode";
 
 function Body(props) {
+  const { signed } = useAuth();
   return (
     <>
       <Header />
-      <div className="relative">
+      {signed && (
+        <div className="relative">
           <Qrcode />
-      </div>
+        </div>
+      )}
       <div className="min-h-screen">{props.children}</div>
       <Footer />
     </>
